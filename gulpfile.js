a34df6ff646677b2e9b834264611e56d4484c6ae
Jan Cafourek <jan.cafourek@gmail.com>
2017-05-02 17:04:31 +0200

    gulp works with bower, bootstrap added

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,13 @@
 var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
+var plugins = require("gulp-load-plugins")({
+	pattern: ['gulp-*', 'gulp.*', 'main-bower-files'],
+	replaceString: /\bgulp[\-.]/
+});
 
+console.log(plugins);
+
+gulp.task('default', function() {
+	return gulp.src(plugins.mainBowerFiles())
+		.pipe(plugins.bowerNormalize({bowerJson: './bower.json'}))
+		.pipe(gulp.dest('./dist/vendor'));
+});
