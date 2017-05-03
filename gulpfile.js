87db883836729798de8fe139810825c2186308d1
Jan Cafourek <jan.cafourek@gmail.com>
2017-05-03 12:28:01 +0200

    changed to bootstrap-css, gulpfile now deletes vendor folder content before generating new

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 var gulp = require('gulp');
 var plugins = require("gulp-load-plugins")({
-	pattern: ['gulp-*', 'gulp.*', 'main-bower-files'],
+	pattern: ['gulp-*', 'gulp.*', 'main-bower-files','del'],
 	replaceString: /\bgulp[\-.]/
 });
 
-console.log(plugins);
+gulp.task('clean_vendor_assets',function(){
+	plugins.del(['dist/vendor/*']);
+})
 
-gulp.task('default', function() {
+gulp.task('bowerify', ['clean_vendor_assets'], function() {
 	return gulp.src(plugins.mainBowerFiles())
 		.pipe(plugins.bowerNormalize({bowerJson: './bower.json'}))
 		.pipe(gulp.dest('./dist/vendor'));
 });
+
+gulp.task('default',['bowerify']);
