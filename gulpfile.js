ef87f086dfa94dca10c1cc7a654a13fc477530d0
Jan Cafourek <jan.cafourek@gmail.com>
2017-05-03 17:58:06 +0200

    order off concat is now by bower.json dependencies - means jquery is concated before bootstrap etc.

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,28 @@ var plugins = require("gulp-load-plugins")({
 	pattern: ['gulp-*', 'gulp.*', 'main-bower-files','del'],
 	replaceString: /\bgulp[\-.]/
 });
+var vendor = require('gulp-concat-vendor');
 
 gulp.task('clean_vendor_assets',function(){
 	plugins.del(['dist/vendor/*']);
 })
 
 gulp.task('bowerify', ['clean_vendor_assets'], function() {
-	return gulp.src(plugins.mainBowerFiles())
-		.pipe(plugins.bowerNormalize({bowerJson: './bower.json'}))
+	return gulp.src(plugins.mainBowerFiles('**/*.js'))
+		.pipe(vendor('libs.min.js'))
 		.pipe(gulp.dest('./dist/vendor'));
 });
 
-gulp.task('default',['bowerify']);
+gulp.task('css',['bowerify'],function(){
+	// return gulp.src(['./dist/vendor/css/*']).
+	// 	pipe(plugins.concat('merged.css')).
+	// 	pipe(gulp.dest('./dist/css'));
+})
+
+gulp.task('js',['bowerify'],function(){
+	// return gulp.src(['./dist/vendor/js/*']).
+	//  	pipe(vendor('merged-libs.js')).
+	//  	pipe(gulp.dest('./dist/js'));	
+})
+
+gulp.task('default',['bowerify','css','js']);
