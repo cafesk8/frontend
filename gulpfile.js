d414d9d1147ef0b7f3b19130c77b279e7e61e515
Jan Cafourek <jan.cafourek@gmail.com>
2017-05-04 16:33:13 +0200

    gulpfile edits

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,34 @@
-var gulp = require('gulp');
-var plugins = require("gulp-load-plugins")({
-	pattern: ['gulp-*', 'gulp.*', 'main-bower-files','del'],
-	replaceString: /\bgulp[\-.]/
-});
-var vendor = require('gulp-concat-vendor');
+var gulp = require('gulp'),
+	plugins = require("gulp-load-plugins")({
+		pattern: ['gulp-*', 'gulp.*', 'main-bower-files','del'],
+		replaceString: /\bgulp[\-.]/
+	}),
+	folder = {
+		src: 'src/',
+		build: 'build/',
+		dist: 'dist/'
+	};
 
 gulp.task('clean_vendor_assets',function(){
-	plugins.del(['build/vendor/*']);
+	plugins.del([folder.build+'*']);
 })
 
 gulp.task('bowerify', ['clean_vendor_assets'], function() {
 	return gulp.src(plugins.mainBowerFiles('**/*.js'))
-		.pipe(vendor('libs.js'))
-		.pipe(gulp.dest('./build/vendor'));
+		.pipe(plugins.concatVendor('libs.js'))
+		.pipe(gulp.dest(folder.build+'vendor'));
 });
 
 gulp.task('css',['bowerify'],function(){
-	// return gulp.src(['./build/vendor/css/*']).
+	// return gulp.src(['./build/plugins.concatVendor/css/*']).
 	// 	pipe(plugins.concat('merged.css')).
 	// 	pipe(gulp.dest('./build/css'));
 })
 
 gulp.task('js',['bowerify'],function(){
-	// return gulp.src(['./build/vendor/js/*']).
-	//  	pipe(vendor('merged-libs.js')).
+	// return gulp.src(['./build/plugins.concatVendor/js/*']).
+	//  	pipe(plugins.concatVendor('merged-libs.js')).
 	//  	pipe(gulp.dest('./build/js'));	
 })
-
-/* Minify / concat etc. to dist folder */
 
 gulp.task('default',['bowerify','css','js']);
